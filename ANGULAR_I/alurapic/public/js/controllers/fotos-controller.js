e3af3bf263f9b68eaf32b0fb85962e8f24b04a1f
Rodolfo Santos <rodolfomoreira88@gmail.com>
2017-11-11 14:53:44 -0200

    Atualização do projeto
    -Utilização do artefato $http
    -Conceito de promisse
    -Inserção de novas fotos na View

--- ANGULAR_I/alurapic/public/js/controllers/fotos-controller.js
+++ ANGULAR_I/alurapic/public/js/controllers/fotos-controller.js
@@ -1,9 +1,18 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
-    $scope.foto = {
-        titulo: 'Rick and Monty',
-        url: 'img/rick and morty.png'
-    };
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+    $scope.fotos = [];
+
+    $http.get('/v1/fotos')
+    .success(function(retorno){
+        console.log(retorno);
+        $scope.fotos = retorno;
+    })
+    .error(function(erro){
+        console.log(erro);
+    });
+    
 });
 
 //O scope recebe propriedades dinamicamente
 //O scope vai disponibilizar dados para a View
+//$http realiza requisição AJAX
+//
